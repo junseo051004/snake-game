39b28f480ee9d25c11b18abd6dd4c5a578f52d7d
junseo051004 <junseo112233@naver.com>
2024-11-28 16:03:24 +0900

    Add files via upload

--- fnc_service.py
+++ fnc_service.py
@@ -1,23 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
 from collect_dao import insert_news
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time 
 
-def collect_news(category: str):
-    page = 1
-    count = 1  
+options = Options()
+options.add_experimental_option("detach", True) 
+# options.add_argument("headless") 
+options.add_argument("disabel-blink-features=AutomationControlled")
+options.add_experimental_option("useAutomationExtension",False)
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+
+
+def collect_news():
+    count = 0
+    url = "https://news.daum.net/"
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    time.sleep(1)
+    
+    # 숙제: 중복 방지 체크
+    # 1p(9개 기사) → Total 3P(27개 기사) → 1번 수집(27개 기사)
+    # 로직
+    # 1. 다음 기사 수집 URL과 url_list의 URL을 비교
+    #   → 있음(중복), 수집 멈춤
+    #   → 없음, 수집 계속
+    # 2. 기사 1건 수집 완료 → 해당 기사 URL을 url_list에 저장
+    
+    
+    
     while True:
-        url = f"https://news.daum.net/breakingnews/{category}?page={page}"
-        result = requests.get(url)
-        doc = BeautifulSoup(result.text, "html.parser")
-        link_list = doc.select("ul.list_news2 a.link_txt")
-        if len(link_list) == 0:
-            break
+        # 9건의 기사 수집
+        doc = BeautifulSoup(driver.page_source, "html.parser")
+        link_list = doc.select("article.content-article ul.list_newsheadline2 a.item_newsheadline2")
         for link in link_list:
             print(f"{count} ===============================================")
             get_news_info(link["href"])
             count+=1
-        page+=1
-       
+            
+        # [새로운 뉴스] 버튼 클릭 이벤트
+        driver.find_element(By.XPATH,'//*[@id="58d84141-b8dd-413c-9500-447b39ec29b9"]/div[2]/a').click()
+        time.sleep(1)
 
 def get_news_info(url: str):
     result = requests.get(url)
@@ -48,4 +74,7 @@ def get_news_info(url: str):
         "regdate":reg_date,
     }
     
-    insert_news(data)
+    # insert_news(data)
+    
+    
+collect_news()
